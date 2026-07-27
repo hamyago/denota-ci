fc2d0e9335f9fd9f8b48152329e99cd59b1a270d
HAMEDE YAGO <hamyago@gmail.com>
2026-07-27 01:27:28 +0000

    Refactor build directory configuration in build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,13 +5,15 @@ allprojects {
     }
 }
 
-rootProject.buildDir = file("../build")
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    project.buildDir = file("${rootProject.buildDir}/${project.name}")
-    project.evaluationDependsOn(":app")
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    layout.buildDirectory.value(newSubprojectBuildDir)
+    evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
